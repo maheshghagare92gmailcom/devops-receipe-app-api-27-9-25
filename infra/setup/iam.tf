3af9b0e4baf7067c8656bc58eb159c6f94c59e1d
mahesh ghagare <maheshghagare92@gmail.com>
2025-09-28 13:14:53 +0530

    1 14 28

---
+++ infra/setup/iam.tf
@@ -0,0 +1,422 @@
+# #########################
+# # Policy for  EFS + EC2 + RDS #
+# #########################
+
+
+# data "aws_iam_policy_document" "infra_combined" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "rds:DescribeDBSubnetGroups",
+#       "rds:DescribeDBInstances",
+#       "rds:CreateDBSubnetGroup",
+#       "rds:DeleteDBSubnetGroup",
+#       "rds:CreateDBInstance",
+#       "rds:DeleteDBInstance",
+#       "rds:ListTagsForResource",
+#       "rds:ModifyDBInstance",
+#       "rds:AddTagsToResource"
+#     ]
+#     resources = ["*"]
+#   }
+
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "ec2:DescribeVpcs",
+#       "ec2:CreateTags",
+#       "ec2:CreateVpc",
+#       "ec2:DeleteVpc",
+#       "ec2:DescribeSecurityGroups",
+#       "ec2:DeleteSubnet",
+#       "ec2:DeleteSecurityGroup",
+#       "ec2:DescribeNetworkInterfaces",
+#       "ec2:DetachInternetGateway",
+#       "ec2:DescribeInternetGateways",
+#       "ec2:DeleteInternetGateway",
+#       "ec2:DetachNetworkInterface",
+#       "ec2:DescribeVpcEndpoints",
+#       "ec2:DescribeRouteTables",
+#       "ec2:DeleteRouteTable",
+#       "ec2:DeleteVpcEndpoints",
+#       "ec2:DisassociateRouteTable",
+#       "ec2:DeleteRoute",
+#       "ec2:DescribePrefixLists",
+#       "ec2:DescribeSubnets",
+#       "ec2:DescribeVpcAttribute",
+#       "ec2:DescribeNetworkAcls",
+#       "ec2:AssociateRouteTable",
+#       "ec2:AuthorizeSecurityGroupIngress",
+#       "ec2:RevokeSecurityGroupEgress",
+#       "ec2:CreateSecurityGroup",
+#       "ec2:AuthorizeSecurityGroupEgress",
+#       "ec2:CreateVpcEndpoint",
+#       "ec2:ModifySubnetAttribute",
+#       "ec2:CreateSubnet",
+#       "ec2:CreateRoute",
+#       "ec2:CreateRouteTable",
+#       "ec2:CreateInternetGateway",
+#       "ec2:AttachInternetGateway",
+#       "ec2:ModifyVpcAttribute",
+#       "ec2:DescribeAccountAttributes",
+#       "ec2:RevokeSecurityGroupIngress",
+#       "ec2:CreateNetworkInterface",
+#       "ec2:DescribeNetworkInterfaces",
+#       "ec2:AttachNetworkInterface",
+#       "ec2:DescribeSecurityGroups",
+#       "ec2:DescribeSubnets",
+#       "ec2:DescribeVpcs",
+#       "ec2:CreateTags",
+#       "ec2:DeleteNetworkInterface",
+#       "ec2:ModifyNetworkInterfaceAttribute",
+#       "ec2:DescribeNetworkInterfaceAttribute"
+#     ]
+#     resources = ["*"]
+#   }
+
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "elasticfilesystem:DescribeFileSystems",
+#       "elasticfilesystem:DescribeAccessPoints",
+#       "elasticfilesystem:DeleteFileSystem",
+#       "elasticfilesystem:DeleteAccessPoint",
+#       "elasticfilesystem:DescribeMountTargets",
+#       "elasticfilesystem:DeleteMountTarget",
+#       "elasticfilesystem:DescribeMountTargetSecurityGroups",
+#       "elasticfilesystem:DescribeLifecycleConfiguration",
+#       "elasticfilesystem:CreateMountTarget",
+#       "elasticfilesystem:CreateAccessPoint",
+#       "elasticfilesystem:CreateFileSystem",
+#       "elasticfilesystem:TagResource",
+#     ]
+#     resources = ["*"]
+#   }
+
+# }
+
+# resource "aws_iam_policy" "infra_combined" {
+#   name        = "${aws_iam_user.cd.name}-infra"
+#   description = "Combined policy for EC2, EFS, and RDS"
+#   policy      = data.aws_iam_policy_document.infra_combined.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "infra_combined" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.infra_combined.arn
+# }
+# #######################################################################
+# # Create IAM user and policies for Continuous Deployment (CD) account #
+# #######################################################################
+
+# resource "aws_iam_user" "cd" {
+#   name = "recipe-app-api-cd"
+# }
+
+# resource "aws_iam_access_key" "cd" {
+#   user = aws_iam_user.cd.name
+# }
+
+# #########################################################
+# # Policy for Teraform backend to S3 and DynamoDB access #
+# #########################################################
+
+# data "aws_iam_policy_document" "tf_backend" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["s3:ListBucket"]
+#     resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+#   }
+
+#   statement {
+#     effect  = "Allow"
+#     actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+#     resources = [
+#       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+#       "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+#     ]
+#   }
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "dynamodb:DescribeTable",
+#       "dynamodb:GetItem",
+#       "dynamodb:PutItem",
+#       "dynamodb:DeleteItem"
+#     ]
+#     resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
+#   }
+# }
+
+# resource "aws_iam_policy" "tf_backend" {
+#   name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+#   description = "Allow user to use S3 and DynamoDB for TF backend resources"
+#   policy      = data.aws_iam_policy_document.tf_backend.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "tf_backend" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.tf_backend.arn
+# }
+
+# #########################
+# # Policy for ECR access #
+# #########################
+
+# data "aws_iam_policy_document" "ecr" {
+#   statement {
+#     effect    = "Allow"
+#     actions   = ["ecr:GetAuthorizationToken"]
+#     resources = ["*"]
+#   }
+
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "ecr:CompleteLayerUpload",
+#       "ecr:UploadLayerPart",
+#       "ecr:InitiateLayerUpload",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:PutImage"
+#     ]
+#     resources = [
+#       aws_ecr_repository.app.arn,
+#       aws_ecr_repository.proxy.arn,
+#     ]
+#   }
+# }
+
+# resource "aws_iam_policy" "ecr" {
+#   name        = "${aws_iam_user.cd.name}-ecr"
+#   description = "Allow user to manage ECR resources"
+#   policy      = data.aws_iam_policy_document.ecr.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "ecr" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.ecr.arn
+# }
+
+# #########################
+# # Policy for ECS access #
+# #########################
+
+# data "aws_iam_policy_document" "ecs" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "ecs:DescribeClusters",
+#       "ecs:DeregisterTaskDefinition",
+#       "ecs:DeleteCluster",
+#       "ecs:DescribeServices",
+#       "ecs:UpdateService",
+#       "ecs:DeleteService",
+#       "ecs:DescribeTaskDefinition",
+#       "ecs:CreateService",
+#       "ecs:RegisterTaskDefinition",
+#       "ecs:CreateCluster",
+#       "ecs:UpdateCluster",
+#       "ecs:TagResource",
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "ecs" {
+#   name        = "${aws_iam_user.cd.name}-ecs"
+#   description = "Allow user to manage ECS resources."
+#   policy      = data.aws_iam_policy_document.ecs.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "ecs" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.ecs.arn
+# }
+
+# #########################
+# # Policy for IAM access #
+# #########################
+
+# data "aws_iam_policy_document" "iam" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "iam:ListInstanceProfilesForRole",
+#       "iam:ListAttachedRolePolicies",
+#       "iam:DeleteRole",
+#       "iam:ListPolicyVersions",
+#       "iam:DeletePolicy",
+#       "iam:DetachRolePolicy",
+#       "iam:ListRolePolicies",
+#       "iam:GetRole",
+#       "iam:GetPolicyVersion",
+#       "iam:GetPolicy",
+#       "iam:CreateRole",
+#       "iam:CreatePolicy",
+#       "iam:AttachRolePolicy",
+#       "iam:TagRole",
+#       "iam:TagPolicy",
+#       "iam:PassRole"
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "iam" {
+#   name        = "${aws_iam_user.cd.name}-iam"
+#   description = "Allow user to manage IAM resources."
+#   policy      = data.aws_iam_policy_document.iam.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "iam" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.iam.arn
+# }
+
+# ################################
+# # Policy for CloudWatch access #
+# ################################
+
+# data "aws_iam_policy_document" "logs" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "logs:DeleteLogGroup",
+#       "logs:DescribeLogGroups",
+#       "logs:CreateLogGroup",
+#       "logs:TagResource",
+#       "logs:ListTagsLogGroup"
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "logs" {
+#   name        = "${aws_iam_user.cd.name}-logs"
+#   description = "Allow user to manage CloudWatch resources."
+#   policy      = data.aws_iam_policy_document.logs.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "logs" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.logs.arn
+# }
+
+# #########################
+# # Policy for ELB access #
+# #########################
+
+# data "aws_iam_policy_document" "elb" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "elasticloadbalancing:DeleteLoadBalancer",
+#       "elasticloadbalancing:DeleteTargetGroup",
+#       "elasticloadbalancing:DeleteListener",
+#       "elasticloadbalancing:DescribeListeners",
+#       "elasticloadbalancing:DescribeLoadBalancerAttributes",
+#       "elasticloadbalancing:DescribeTargetGroups",
+#       "elasticloadbalancing:DescribeTargetGroupAttributes",
+#       "elasticloadbalancing:DescribeLoadBalancers",
+#       "elasticloadbalancing:CreateListener",
+#       "elasticloadbalancing:SetSecurityGroups",
+#       "elasticloadbalancing:ModifyLoadBalancerAttributes",
+#       "elasticloadbalancing:CreateLoadBalancer",
+#       "elasticloadbalancing:ModifyTargetGroupAttributes",
+#       "elasticloadbalancing:CreateTargetGroup",
+#       "elasticloadbalancing:AddTags",
+#       "elasticloadbalancing:DescribeTags",
+#       "elasticloadbalancing:ModifyListener"
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "elb" {
+#   name        = "${aws_iam_user.cd.name}-elb"
+#   description = "Allow user to manage ELB resources."
+#   policy      = data.aws_iam_policy_document.elb.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "elb" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.elb.arn
+# }
+
+
+# ##############################
+# # Policy for Service-Linked Roles creation #
+# ##############################
+
+# data "aws_iam_policy_document" "service_linked_roles" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "iam:CreateServiceLinkedRole",
+#       "iam:DeleteServiceLinkedRole"
+#     ]
+#     resources = ["*"]
+#     condition {
+#       test     = "StringEquals"
+#       variable = "iam:AWSServiceName"
+#       values = [
+#         "elasticloadbalancing.amazonaws.com",
+#         "rds.amazonaws.com",
+#         "ecs.amazonaws.com"
+#       ]
+#     }
+#   }
+# }
+
+# resource "aws_iam_policy" "service_linked_roles" {
+#   name        = "${aws_iam_user.cd.name}-service-linked-roles"
+#   description = "Allow creation of service-linked roles for ELB and RDS."
+#   policy      = data.aws_iam_policy_document.service_linked_roles.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "service_linked_roles" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.service_linked_roles.arn
+# }
+
+
+
+
+
+# #############################
+# # Policy for Route53 access #
+# #############################
+
+# data "aws_iam_policy_document" "route53" {
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "route53:ListHostedZones",
+#       "route53:ListHostedZones",
+#       "route53:ChangeTagsForResource",
+#       "route53:GetHostedZone",
+#       "route53:ListTagsForResource",
+#       "route53:ChangeResourceRecordSets",
+#       "route53:GetChange",
+#       "route53:ListResourceRecordSets",
+#       "acm:RequestCertificate",
+#       "acm:AddTagsToCertificate",
+#       "acm:DescribeCertificate",
+#       "acm:ListTagsForCertificate",
+#       "acm:DeleteCertificate",
+#       "acm:CreateCertificate"
+#     ]
+#     resources = ["*"]
+#   }
+# }
+
+# resource "aws_iam_policy" "route53" {
+#   name        = "${aws_iam_user.cd.name}-route53"
+#   description = "Allow user to manage Route53 resources."
+#   policy      = data.aws_iam_policy_document.route53.json
+# }
+
+# resource "aws_iam_user_policy_attachment" "route53" {
+#   user       = aws_iam_user.cd.name
+#   policy_arn = aws_iam_policy.route53.arn
+# }
---
+++ infra/setup/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "devops-recipe-app-tf-state-124"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-recipe-app-api-tf-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Project     = var.project
+      Contact     = var.contact
+      ManageBy    = "Terraform/setup"
+    }
+  }
+}
+
+
+# resource "aws_iam_service_linked_role" "rds" {
+#   aws_service_name = "rds.amazonaws.com"
+# }
+
+# resource "aws_iam_service_linked_role" "elb" {
+#   aws_service_name = "elasticloadbalancing.amazonaws.com"
+# }
+
+# resource "aws_iam_service_linked_role" "ecs" {
+#   aws_service_name = "ecs.amazonaws.com"
+# }
