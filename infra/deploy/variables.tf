ac876343668e5be439849363b1b418c23f24629c
mahesh ghagare <maheshghagare92@gmail.com>
2025-10-01 23:59:46 +0530

    11 59 1

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -13,14 +13,14 @@ variable "contact" {
   default     = "mark@example.com"
 }
 
-# variable "db_username" {
-#   description = "Username for the recipe app api database"
-#   default     = "recipeapp"
-# }
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
 
-# variable "db_password" {
-#   description = "Password for the Terraform database"
-# }
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
 
 # variable "ecr_proxy_image" {
 #   description = "Path to the ECR repo with the proxy image"
