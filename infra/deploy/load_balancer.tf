2d054ef439c180b975eec36ddd3c144903102cb2
mahesh ghagare <maheshghagare92@gmail.com>
2025-10-04 01:19:32 +0530

    12 51 4

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -53,23 +53,23 @@ resource "aws_lb_listener" "api" {
   port              = 80
   protocol          = "HTTP"
 
-  #   default_action {
-  #     type = "redirect"
+  default_action {
+    type = "redirect"
 
-  #     redirect {
-  #       port        = "443"
-  #       protocol    = "HTTPS"
-  #       status_code = "HTTP_301"
-  #     }
-  #   }
-  # }
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
-  # resource "aws_lb_listener" "api_https" {
-  #   load_balancer_arn = aws_lb.api.arn
-  #   port              = 443
-  #   protocol          = "HTTPS"
+resource "aws_lb_listener" "api_https" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 443
+  protocol          = "HTTPS"
 
-  #   certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+  certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
 
   default_action {
     type             = "forward"
