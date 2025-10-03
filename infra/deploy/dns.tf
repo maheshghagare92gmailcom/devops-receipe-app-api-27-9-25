50ae2b52518aa18a0c1f0a46b9d785a116ddaae2
mahesh ghagare <maheshghagare92@gmail.com>
2025-10-04 00:44:56 +0530

    12 44 4

--- infra/deploy/dns.tf
+++ infra/deploy/dns.tf
@@ -1,15 +1,15 @@
-# data "aws_route53_zone" "zone" {
-#   name = "${var.dns_zone_name}."
-# }
+data "aws_route53_zone" "zone" {
+  name = "${var.dns_zone_name}."
+}
 
-# resource "aws_route53_record" "app" {
-#   zone_id = data.aws_route53_zone.zone.zone_id
-#   name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
-#   type    = "CNAME"
-#   ttl     = "300"
+resource "aws_route53_record" "app" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${lookup(var.subdomain, terraform.workspace)}.${data.aws_route53_zone.zone.name}"
+  type    = "CNAME"
+  ttl     = "300"
 
-#   records = [aws_lb.api.dns_name]
-# }
+  records = [aws_lb.api.dns_name]
+}
 
 # resource "aws_acm_certificate" "cert" {
 #   domain_name       = aws_route53_record.app.name
