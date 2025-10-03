8b73003ab0ffef6f034f86372e63f00dd4442f04
mahesh ghagare <maheshghagare92@gmail.com>
2025-10-03 14:40:23 +0530

    2 40 3

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -1,33 +1,33 @@
-# #################
-# # Load Balancer #
-# #################
+#################
+# Load Balancer #
+#################
 
-# resource "aws_security_group" "lb" {
-#   description = "Configure access for the Application Load Balancer"
-#   name        = "${local.prefix}-alb-access"
-#   vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "lb" {
+  description = "Configure access for the Application Load Balancer"
+  name        = "${local.prefix}-alb-access"
+  vpc_id      = aws_vpc.main.id
 
-#   ingress {
-#     protocol    = "tcp"
-#     from_port   = 80
-#     to_port     = 80
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     protocol    = "tcp"
-#     from_port   = 443
-#     to_port     = 443
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     protocol    = "tcp"
-#     from_port   = 8000
-#     to_port     = 8000
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+  egress {
+    protocol    = "tcp"
+    from_port   = 8000
+    to_port     = 8000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 # resource "aws_lb" "api" {
 #   name               = "${local.prefix}-lb"
