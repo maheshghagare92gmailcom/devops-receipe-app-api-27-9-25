0c00462ee1ae1e8c10a0dad7fad23e2953569a44
mahesh ghagare <maheshghagare92@gmail.com>
2025-10-03 16:21:48 +0530

    4 21 3

--- infra/deploy/load_balancer.tf
+++ infra/deploy/load_balancer.tf
@@ -29,50 +29,50 @@ resource "aws_security_group" "lb" {
   }
 }
 
-# resource "aws_lb" "api" {
-#   name               = "${local.prefix}-lb"
-#   load_balancer_type = "application"
-#   subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-#   security_groups    = [aws_security_group.lb.id]
-# }
+resource "aws_lb" "api" {
+  name               = "${local.prefix}-lb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.lb.id]
+}
 
-# resource "aws_lb_target_group" "api" {
-#   name        = "${local.prefix}-api"
-#   protocol    = "HTTP"
-#   vpc_id      = aws_vpc.main.id
-#   target_type = "ip"
-#   port        = 8000
+resource "aws_lb_target_group" "api" {
+  name        = "${local.prefix}-api"
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+  port        = 8000
 
-#   health_check {
-#     path = "/api/health-check/"
-#   }
-# }
+  health_check {
+    path = "/api/health-check/"
+  }
+}
 
-# resource "aws_lb_listener" "api" {
-#   load_balancer_arn = aws_lb.api.arn
-#   port              = 80
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "api" {
+  load_balancer_arn = aws_lb.api.arn
+  port              = 80
+  protocol          = "HTTP"
 
-#   default_action {
-#     type = "redirect"
+  #   default_action {
+  #     type = "redirect"
 
-#     redirect {
-#       port        = "443"
-#       protocol    = "HTTPS"
-#       status_code = "HTTP_301"
-#     }
-#   }
-# }
+  #     redirect {
+  #       port        = "443"
+  #       protocol    = "HTTPS"
+  #       status_code = "HTTP_301"
+  #     }
+  #   }
+  # }
 
-# resource "aws_lb_listener" "api_https" {
-#   load_balancer_arn = aws_lb.api.arn
-#   port              = 443
-#   protocol          = "HTTPS"
+  # resource "aws_lb_listener" "api_https" {
+  #   load_balancer_arn = aws_lb.api.arn
+  #   port              = 443
+  #   protocol          = "HTTPS"
 
-#   certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+  #   certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.api.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
